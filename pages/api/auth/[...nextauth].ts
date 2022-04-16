f41fc11e1889a50e74ca80f7bc2c817f758c21c4
tearaku <teacup1592@gmail.com>
2022-04-16 22:15:35 +0800

    working localhost FB login, updates to user profile; init commit

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,42 @@
+import NextAuth from "next-auth"
+import GoogleProvider from "next-auth/providers/google"
+import FacebookProvider from "next-auth/providers/facebook"
+import { PrismaAdapter } from "@next-auth/prisma-adapter"
+import prisma from "../../../utils/prisma"
+
+// For more information on each option (and a full list of options) go to
+// https://next-auth.js.org/configuration/options
+export default NextAuth({
+  // https://next-auth.js.org/configuration/providers/oauth
+  providers: [
+    FacebookProvider({
+      clientId: process.env.FACEBOOK_ID_DEV as string,
+      clientSecret: process.env.FACEBOOK_SECRET_DEV as string,
+    }),
+    // GoogleProvider({
+    //   clientId: process.env.GOOGLE_ID as string,
+    //   clientSecret: process.env.GOOGLE_SECRET as string,
+    // }),
+  ],
+  theme: {
+    colorScheme: "light",
+  },
+  pages: {
+    newUser: '/profile',
+  },
+  callbacks: {
+    async session({ session, token, user }) {
+      // Send properties to the client, like an access_token from a provider.
+      const userData = await prisma.user.findUnique({
+        where: {
+          email: session.user.email,
+        }
+      })
+      session.user.id = userData.id
+      console.log(session)
+      return session
+    }
+  },
+  adapter: PrismaAdapter(prisma),
+  secret: process.env.SECRET,
+})
